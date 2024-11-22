9132708032293db2a3a8365297ad8624b7e4be27
vinimacar <132243328+vinimacar@users.noreply.github.com>
2024-11-22 13:49:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -205,3 +205,17 @@ window.onload = function() {
     fillBooksTable();
     fillUsersTable();
 };
+const viewDate = document.querySelector(".date");
+const viewTime = document.querySelector(".time");
+
+const fullDate = new Date();
+const day = fullDate.toLocaleString("pt-BR", { weekday: "long" });
+const formatedDay = day.charAt(0).toUpperCase() + day.slice(1);
+
+const hours = String(fullDate.getHours()).padStart(2, "0");
+const minutes = String(fullDate.getMinutes()).padStart(2, "0");
+const seconds = String(fullDate.getSeconds()).padStart(2, "0");
+const fullTime = `${hours}:${minutes}:${seconds}`;
+
+viewDate.textContent = formatedDay;
+viewTime.textContent = fullTime;
