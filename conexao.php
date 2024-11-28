e23c9cc27fe96588e10852714167979718ed0920
vinimacar <132243328+vinimacar@users.noreply.github.com>
2024-11-28 09:10:10 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 $servername = "127.0.0.1";  // ou o endereço do servidor de banco de dados
 $username = "root";         // seu nome de usuário MySQL
 $password = "";             // sua senha MySQL
-$dbname = "escolasDB";      // nome do seu banco de dados
+$dbname = "gebe";      // nome do seu banco de dados
 
 // Criando a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
